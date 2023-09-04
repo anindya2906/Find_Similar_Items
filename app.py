4d99e143ce5fe9ee7f7a83bb40cb514de7c23beb
Anindya Saha <anindya_2906@yahoo.co.in>
2023-09-04 14:15:55 +0000

    Test FAISS

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from langchain.document_loaders.csv_loader import CSVLoader
 
 load_dotenv()
 
-def load_csv_data(csv_file):
+openai_embd = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+def upload_csv_file(csv_file):
     """Load csv data
     """
     loader = CSVLoader(file_path=csv_file, csv_args={
@@ -22,16 +24,30 @@ def load_csv_data(csv_file):
     return data
 
 
+def save_uploaded_file(file):
+    """Save uploaded file
+    """
+    file_bytes_data = file.getvalue()
+    temp_file_path = f"./example_data/{file.name}"
+    with open(temp_file_path, "wb") as f:
+        f.write(file_bytes_data)
+    return temp_file_path
+
+
+def create_faiss_vectorstore(data):
+    """Create FAISS index
+    """
+    faiss_vector_store = FAISS.from_documents(data, openai_embd)
+    return faiss_vector_store
+
+
 st.set_page_config(page_title="Find Similar Item with LLM Embeddings")
 st.header("Find Similar Items with LLM Embeddings")
 
 uploaded_file = st.file_uploader("Upload CSV: ", type=["csv"])
 
 if uploaded_file is not None:
-    file_bytes_data = uploaded_file.getvalue()
-    temp_file_path = f"./example_data/{uploaded_file.name}"
-    with open(temp_file_path, "wb") as f:
-        f.write(file_bytes_data)
-
-    load_csv_data(temp_file_path)
-    st.write(uploaded_file.name)
+    temp_file_path = save_uploaded_file(uploaded_file)
+    file_data = upload_csv_file(temp_file_path)
+    faiss_vector_store = create_faiss_vectorstore(file_data)
+    st.write("Done")
